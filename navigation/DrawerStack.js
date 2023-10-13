8b0887140a35d94c6d8f45b4085551a6e322858f
Anand Stephan <anandstephan98@gmail.com>
2023-10-13 08:29:41 +0530

    feat:add common component

--- navigation/DrawerStack.js
+++ navigation/DrawerStack.js
@@ -1,12 +1,25 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../screens/Home';
-
+import { Text } from 'native-base';
+import SidebarHeader from '../components/common/SidebarHeader';
 const Drawer = createDrawerNavigator();
 
 const DrawerStack = () => {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+    screenOptions={{
+        headerStatusBarHeight:5,
+        headerTitle:"",
+        headerRight:()=><SidebarHeader/>,
+    }}
+    
+    >
       <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Profile" component={Home} />
+      <Drawer.Screen name="History" component={Home} />
+      <Drawer.Screen name="AboutUs" component={Home}/>
+      <Drawer.Screen name="Logout" component={Home}/>
+      
     </Drawer.Navigator>
   );
 }
